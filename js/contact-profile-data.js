a7ae2c485166bbad89d758ef252f889fbdd31cd5
Ridoy Sharif <developersridoy@gmail.com>
2023-03-04 15:46:17 +0600

    contact profile area design and created contact profile data

---
+++ js/contact-profile-data.js
@@ -0,0 +1,43 @@
+const facebookUser = [
+   {
+      img: "https://scontent.fjsr2-1.fna.fbcdn.net/v/t39.30808-6/310661395_2979277039031852_2139936212626349265_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeG-dNzxooBziEdTp-KVIF0k6lJCdZS8z-PqUkJ1lLzP49HLmK0dOWBPGoK5wfQeWiWitTMsXEnCTFssR5cl4TKb&_nc_ohc=yhzjx6lN5K0AX9bEoCg&_nc_zt=23&_nc_ht=scontent.fjsr2-1.fna&oh=00_AfDJBCZCw3wvNjrDq1b0HQ1eALMK7NDw84V7poVA9jrJlg&oe=6407EB6B",
+      name: "Sabbir Hossain"
+   },
+   {
+      img: "https://scontent.fjsr1-1.fna.fbcdn.net/v/t39.30808-6/323057208_556665342747664_1700868055197723540_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=174925&_nc_eui2=AeGQycshM6iv9n40yECRn4m-bKyvRl55D0tsrK9GXnkPS14ppT-ufWAngTDfX603IvA-QEcdLM0o0WuqnrWbAZJr&_nc_ohc=O14Gl5cCbH8AX8TMikY&_nc_zt=23&_nc_ht=scontent.fjsr1-1.fna&oh=00_AfDhjdf2ie4a9Vz6Tpj_tMTBiwfJnjcMCcPvuhN4YxmgSw&oe=6407CCF5",
+      name: "RI Anik"
+   },
+   {
+      img: "https://scontent.fjsr2-1.fna.fbcdn.net/v/t39.30808-6/271002622_1360831414375237_8279833029187531204_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeHo4blADCVLSp4YtMhrFLnux-vGqu6b2Q3H68aq7pvZDd-FPIwFbv1xp7kdqwXY5kYItuXWd71xXiF3j8S12NZ4&_nc_ohc=dGCqbI_A7TUAX_0VlB-&_nc_oc=AQm2ncg7sYXl4_bXJWocXjhcm5a2T4HYMYxtpAY8iR25pH5i3Ni-AMlFRQahr1TgLNs&_nc_zt=23&_nc_ht=scontent.fjsr2-1.fna&oh=00_AfCjPWmD6kZA-zBqCiL1u1MhM151e5yLT7F4YPgCC3ogxQ&oe=6408C148",
+      name: "Naimul Khan"
+   },
+   {
+      img: "https://scontent.fjsr1-1.fna.fbcdn.net/v/t39.30808-6/325741019_701927981533477_8803385225124829888_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeHm1JjrngJ7tHNJbMvnN4SvFtDDWCtL2UIW0MNYK0vZQjjxSZB-HSskWTTz4unk7dzypn72KR_VvZlksm4VhGVy&_nc_ohc=qlvhC3ljrHgAX_VfPiU&_nc_zt=23&_nc_ht=scontent.fjsr1-1.fna&oh=00_AfD1Tbwhc13_dqlAt8QGWWg4zpP5dCEG7iFCTh1jid5krA&oe=64087F81",
+      name: "রেজওয়ানুল হক"
+   },
+   {
+      img: "https://scontent.fjsr2-1.fna.fbcdn.net/v/t39.30808-1/331233114_1329266207644491_6119531610762788833_n.jpg?stp=dst-jpg_p200x200&_nc_cat=108&ccb=1-7&_nc_sid=f67be1&_nc_eui2=AeH2OcPUT6FK-lW8PDW4cOTIAjDMYKri-Z0CMMxgquL5nQocht6jfVDtygdL2kynBgjxclveXy4nFOpvQD0oWkeJ&_nc_ohc=pfQB3t7L0xcAX_c0Hle&_nc_ht=scontent.fjsr2-1.fna&oh=00_AfBCJmW3zOoP30eZTVyBu7uZjgqD-heO3rxeyVL1WIvMSw&oe=640823BD",
+      name: "Md. Nur Alam Mia"
+   },
+   {
+      img: "https://scontent.fjsr1-1.fna.fbcdn.net/v/t39.30808-6/299104343_583121499977649_4748635239627842007_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeH10kftyBcmlkIktYVurZcFr3dE5gqNdcevd0TmCo11x51NhRN_6RXLVK-xgoxFwCiyLWgq4Eaj__bG3w5AzqJb&_nc_ohc=8-OsjJSwak0AX-WP6hV&_nc_zt=23&_nc_ht=scontent.fjsr1-1.fna&oh=00_AfDoMO-JQV4UI1lHNph--Vi5fuAvFgbh2VNS_3T69swlqA&oe=6408A758",
+      name: "Md Rana Bokshe"
+   },
+   {
+      img: "https://scontent.fjsr2-1.fna.fbcdn.net/v/t1.6435-9/36199718_272920476617544_4170556545999831040_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFkE1V9zUHhNoXqRlifErPzFRtJPdxQTxgVG0k93FBPGIjm1K8tFiPMlKKu2cFtHFfYP5gKN4aMOroPih3GdAKt&_nc_ohc=SvEbjiScrZ8AX-aZy3G&_nc_ht=scontent.fjsr2-1.fna&oh=00_AfA41k__gX5SHUipfKQsyGox7OfLQiGDPWzRDMs5PbUE1Q&oe=642A703B",
+      name: "Jewel Mondol"
+   },
+   {
+      img: "https://scontent.fjsr2-1.fna.fbcdn.net/v/t39.30808-1/325899415_696453128870613_5839177009954100353_n.jpg?stp=dst-jpg_s200x200&_nc_cat=109&ccb=1-7&_nc_sid=f67be1&_nc_eui2=AeEcaUPB7QLeVnq_H-ctnwYig1CnCxRVDkmDUKcLFFUOSQcUrL9tU6dW1QpulSEjPhiiyK64OpZb2TbBkMGjiF9E&_nc_ohc=A-inMidesA8AX8Mu7H2&_nc_ht=scontent.fjsr2-1.fna&oh=00_AfAnNSvbUNAlj9YnnjVe_9tLIPUPoWq_xUikeLVnFXbr5g&oe=64084DCB",
+      name: "Md Emon Hossen"
+   },
+   {
+      img: "https://scontent.fjsr1-1.fna.fbcdn.net/v/t39.30808-1/327029999_1807534936286218_7312908698144588623_n.jpg?stp=c11.0.200.200a_dst-jpg_p200x200&_nc_cat=100&ccb=1-7&_nc_sid=7206a8&_nc_eui2=AeFUBJh_eTvB0tAt67T-Npv9OtGPglX3fPU60Y-CVfd89aktbsK__Jybrt_-N-m-p9Pc0Ap6VspbWBM5BN8ZxgP-&_nc_ohc=cpY_T5Gy5sYAX8SwxdM&_nc_ht=scontent.fjsr1-1.fna&oh=00_AfCV5OK0KzVeBEv8j8otZQ2e-8ikmH_JNdpnGwVEBIAcQw&oe=640751CF",
+      name: "মুহাম্মদ আরিফুজ্জামান"
+   },
+   {
+      img: "https://scontent.fjsr2-1.fna.fbcdn.net/v/t39.30808-6/324115662_950282485965820_9008497820099236372_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeEpGHbYWK3iyNE7Yt_fLexCMaT7DZ9pJgYxpPsNn2kmBqlaKlVp8HhOQZr8V0vk7UyxitoU5OWEL-3zGUGytLiJ&_nc_ohc=zYfr1L0gUfUAX-sWbTc&_nc_zt=23&_nc_ht=scontent.fjsr2-1.fna&oh=00_AfAD8QZ8Ff5_cp0fKzemTcurDpWdfY0zAwQkKRywR_e-kA&oe=64089335",
+      name: "Abu Bakar Muhammad Yahya"
+   }
+
+]
