b596c8d1c11a9cbf77f77754e88e5e8571c25eae
Ridoy Sharif <developersridoy@gmail.com>
2023-03-04 17:42:32 +0600

    contact profile showing display

--- js/contact-profile-data.js
+++ js/contact-profile-data.js
@@ -1,7 +1,15 @@
 const facebookUser = [
    {
+      img: "https://scontent.fjsr2-1.fna.fbcdn.net/v/t1.6435-9/36199718_272920476617544_4170556545999831040_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFkE1V9zUHhNoXqRlifErPzFRtJPdxQTxgVG0k93FBPGIjm1K8tFiPMlKKu2cFtHFfYP5gKN4aMOroPih3GdAKt&_nc_ohc=SvEbjiScrZ8AX-aZy3G&_nc_ht=scontent.fjsr2-1.fna&oh=00_AfA41k__gX5SHUipfKQsyGox7OfLQiGDPWzRDMs5PbUE1Q&oe=642A703B",
+      name: "Jewel Mondol"
+   },
+   {
       img: "https://scontent.fjsr2-1.fna.fbcdn.net/v/t39.30808-6/310661395_2979277039031852_2139936212626349265_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeG-dNzxooBziEdTp-KVIF0k6lJCdZS8z-PqUkJ1lLzP49HLmK0dOWBPGoK5wfQeWiWitTMsXEnCTFssR5cl4TKb&_nc_ohc=yhzjx6lN5K0AX9bEoCg&_nc_zt=23&_nc_ht=scontent.fjsr2-1.fna&oh=00_AfDJBCZCw3wvNjrDq1b0HQ1eALMK7NDw84V7poVA9jrJlg&oe=6407EB6B",
       name: "Sabbir Hossain"
+   },
+   {
+      img: "https://scontent.fjsr1-1.fna.fbcdn.net/v/t39.30808-6/325741019_701927981533477_8803385225124829888_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeHm1JjrngJ7tHNJbMvnN4SvFtDDWCtL2UIW0MNYK0vZQjjxSZB-HSskWTTz4unk7dzypn72KR_VvZlksm4VhGVy&_nc_ohc=qlvhC3ljrHgAX_VfPiU&_nc_zt=23&_nc_ht=scontent.fjsr1-1.fna&oh=00_AfD1Tbwhc13_dqlAt8QGWWg4zpP5dCEG7iFCTh1jid5krA&oe=64087F81",
+      name: "রেজওয়ানুল হক"
    },
    {
       img: "https://scontent.fjsr1-1.fna.fbcdn.net/v/t39.30808-6/323057208_556665342747664_1700868055197723540_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=174925&_nc_eui2=AeGQycshM6iv9n40yECRn4m-bKyvRl55D0tsrK9GXnkPS14ppT-ufWAngTDfX603IvA-QEcdLM0o0WuqnrWbAZJr&_nc_ohc=O14Gl5cCbH8AX8TMikY&_nc_zt=23&_nc_ht=scontent.fjsr1-1.fna&oh=00_AfDhjdf2ie4a9Vz6Tpj_tMTBiwfJnjcMCcPvuhN4YxmgSw&oe=6407CCF5",
@@ -12,8 +20,24 @@ const facebookUser = [
       name: "Naimul Khan"
    },
    {
-      img: "https://scontent.fjsr1-1.fna.fbcdn.net/v/t39.30808-6/325741019_701927981533477_8803385225124829888_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeHm1JjrngJ7tHNJbMvnN4SvFtDDWCtL2UIW0MNYK0vZQjjxSZB-HSskWTTz4unk7dzypn72KR_VvZlksm4VhGVy&_nc_ohc=qlvhC3ljrHgAX_VfPiU&_nc_zt=23&_nc_ht=scontent.fjsr1-1.fna&oh=00_AfD1Tbwhc13_dqlAt8QGWWg4zpP5dCEG7iFCTh1jid5krA&oe=64087F81",
-      name: "রেজওয়ানুল হক"
+      img: "https://scontent.fjsr1-1.fna.fbcdn.net/v/t39.30808-6/330326510_702818448189513_7311095488179111122_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGqGIXPWXpSmmaYq8gBrwxTLgJOKLKaRSkuAk4osppFKWO0DBlp1NLNyHMPhLsdOqkbLPajtT72GJRWxVGhA_ly&_nc_ohc=kfjIMoKIDM8AX-P1jmY&_nc_zt=23&_nc_ht=scontent.fjsr1-1.fna&oh=00_AfAqf8frUs-2-5sQ9uPpzf3UcMjLliXkSxMTdVNIhdl23Q&oe=64087610",
+      name: "Royel Khan"
+   },
+   {
+      img: "https://scontent.fjsr1-1.fna.fbcdn.net/v/t1.6435-9/164240409_284584009702553_7819105316886622037_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=174925&_nc_eui2=AeHAi2Wn0EYdtUfR0R7cziaS09XG62s3MNDT1cbrazcw0DMwuEjcQe_Mh4nqFdRtw8HP1lZlzozp85N6o0Ue8W7G&_nc_ohc=QdJfwa-TzpAAX_1Vbw8&_nc_ht=scontent.fjsr1-1.fna&oh=00_AfCo3iDVlx55z6_i6s_BWOBc11OdKFY2l_14nQobgovDsA&oe=642AB78B",
+      name: "Bijoy Ahamed"
+   },
+   {
+      img: "https://scontent.fjsr1-1.fna.fbcdn.net/v/t39.30808-6/313865778_635467041381394_5271878217468691537_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFZ_diKHnT8hxG5lZDSiKU6-w69tO5MR5X7Dr207kxHleJ_-9oAQqesHoY7Mub_6e4SuaUhNWBJgz-fxciNbDyT&_nc_ohc=7l9AV7sDrksAX97We98&_nc_zt=23&_nc_ht=scontent.fjsr1-1.fna&oh=00_AfDxiDHVNbppzb9j5EW8YKbaJK3r9MwK23dQusG_iv7cUg&oe=6408A322",
+      name: "Md Imran Hasan"
+   },
+   {
+      img: "https://scontent.fdac2-1.fna.fbcdn.net/v/t39.30808-6/275700753_1366544673812021_5045414966375856315_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFhYtqoOXxPuHdjLh0r7FUw8jkP2k9K1ILyOQ_aT0rUgnbeHU1mYKii4vyZP8QdZZqKm50BMaHS01Kz56LLlmA-&_nc_ohc=CeRDTWkBb2UAX_Zl1ht&_nc_zt=23&_nc_ht=scontent.fdac2-1.fna&oh=00_AfBnsVv4pQWQ6mximu0djbI3kfyocLwyvD0_D8UuR2YKGA&oe=640731DC",
+      name: "Sheik Shihab Shahariar Somrat"
+   },
+   {
+      img: "https://scontent.fjsr1-1.fna.fbcdn.net/v/t39.30808-6/315110610_802499330859473_5605934913661609401_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFV7IaZtYtqgQ_N3ABMpjocTCgTRfJo53VMKBNF8mjndZCUSAZWQLSMuiVtdFMmhtDT4NM8iLfrakeagUKEYgfT&_nc_ohc=r9_kgj0lXpQAX978I7x&_nc_zt=23&_nc_ht=scontent.fjsr1-1.fna&oh=00_AfDd4fnGsCZdSKzBhyxeVV9BmYScTY2VNwn689sy_Uc8hg&oe=64081CD0",
+      name: "Md Munna Miea"
    },
    {
       img: "https://scontent.fjsr2-1.fna.fbcdn.net/v/t39.30808-1/331233114_1329266207644491_6119531610762788833_n.jpg?stp=dst-jpg_p200x200&_nc_cat=108&ccb=1-7&_nc_sid=f67be1&_nc_eui2=AeH2OcPUT6FK-lW8PDW4cOTIAjDMYKri-Z0CMMxgquL5nQocht6jfVDtygdL2kynBgjxclveXy4nFOpvQD0oWkeJ&_nc_ohc=pfQB3t7L0xcAX_c0Hle&_nc_ht=scontent.fjsr2-1.fna&oh=00_AfBCJmW3zOoP30eZTVyBu7uZjgqD-heO3rxeyVL1WIvMSw&oe=640823BD",
@@ -22,10 +46,6 @@ const facebookUser = [
    {
       img: "https://scontent.fjsr1-1.fna.fbcdn.net/v/t39.30808-6/299104343_583121499977649_4748635239627842007_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeH10kftyBcmlkIktYVurZcFr3dE5gqNdcevd0TmCo11x51NhRN_6RXLVK-xgoxFwCiyLWgq4Eaj__bG3w5AzqJb&_nc_ohc=8-OsjJSwak0AX-WP6hV&_nc_zt=23&_nc_ht=scontent.fjsr1-1.fna&oh=00_AfDoMO-JQV4UI1lHNph--Vi5fuAvFgbh2VNS_3T69swlqA&oe=6408A758",
       name: "Md Rana Bokshe"
-   },
-   {
-      img: "https://scontent.fjsr2-1.fna.fbcdn.net/v/t1.6435-9/36199718_272920476617544_4170556545999831040_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFkE1V9zUHhNoXqRlifErPzFRtJPdxQTxgVG0k93FBPGIjm1K8tFiPMlKKu2cFtHFfYP5gKN4aMOroPih3GdAKt&_nc_ohc=SvEbjiScrZ8AX-aZy3G&_nc_ht=scontent.fjsr2-1.fna&oh=00_AfA41k__gX5SHUipfKQsyGox7OfLQiGDPWzRDMs5PbUE1Q&oe=642A703B",
-      name: "Jewel Mondol"
    },
    {
       img: "https://scontent.fjsr2-1.fna.fbcdn.net/v/t39.30808-1/325899415_696453128870613_5839177009954100353_n.jpg?stp=dst-jpg_s200x200&_nc_cat=109&ccb=1-7&_nc_sid=f67be1&_nc_eui2=AeEcaUPB7QLeVnq_H-ctnwYig1CnCxRVDkmDUKcLFFUOSQcUrL9tU6dW1QpulSEjPhiiyK64OpZb2TbBkMGjiF9E&_nc_ohc=A-inMidesA8AX8Mu7H2&_nc_ht=scontent.fjsr2-1.fna&oh=00_AfAnNSvbUNAlj9YnnjVe_9tLIPUPoWq_xUikeLVnFXbr5g&oe=64084DCB",
@@ -38,6 +58,27 @@ const facebookUser = [
    {
       img: "https://scontent.fjsr2-1.fna.fbcdn.net/v/t39.30808-6/324115662_950282485965820_9008497820099236372_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeEpGHbYWK3iyNE7Yt_fLexCMaT7DZ9pJgYxpPsNn2kmBqlaKlVp8HhOQZr8V0vk7UyxitoU5OWEL-3zGUGytLiJ&_nc_ohc=zYfr1L0gUfUAX-sWbTc&_nc_zt=23&_nc_ht=scontent.fjsr2-1.fna&oh=00_AfAD8QZ8Ff5_cp0fKzemTcurDpWdfY0zAwQkKRywR_e-kA&oe=64089335",
       name: "Abu Bakar Muhammad Yahya"
-   }
-
+   },
+   {
+      img: "https://scontent.fjsr1-1.fna.fbcdn.net/v/t39.30808-6/278601126_516414170082784_6609495775667935831_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeH-tzaESwdPyqraiZI9Mq1Ser20hfKUShV6vbSF8pRKFdPAeyVYrPAxgOZsdrbFd5zzbS1etJBg85YWfTbrTZ0S&_nc_ohc=P2DKtD5qEFoAX8PtQn8&_nc_zt=23&_nc_ht=scontent.fjsr1-1.fna&oh=00_AfAXOEvjeX0ShSqGFDxbSleWBivhU3aedSEsvOs5_t8uwA&oe=64088A08",
+      name: "Md Moshikur Rahaman"
+   },
+   {
+      img: "https://scontent.fjsr1-1.fna.fbcdn.net/v/t39.30808-6/244548185_1291927697913722_3967331307473980621_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGwhnrtg1e4Qv70uP4Dtf2is402VYMv-VCzjTZVgy_5UEXwAV87_COZjKNDjGU9YZ0XcFhGBNvCWa6BY3YhPlab&_nc_ohc=Tiw2P7ql7pYAX8cPkqd&_nc_zt=23&_nc_ht=scontent.fjsr1-1.fna&oh=00_AfAISewbhXeTFGGVhPzGksVUzKaFgn_ylVgAspxA0GEvag&oe=640896C5",
+      name: "Md. Alamin Mia"
+   },
 ]
+
+const contactProfile = document.getElementById("contact-profile")
+facebookUser.map(user => {
+   contactProfile.innerHTML += `
+   <a href="#" class="flex items-center gap-3 mb-2 py-2 px-2 rounded-md hover:bg-[#E4E6E9] duration-500">
+      <li class="list-none">
+         <img class="w-8 h-8 rounded-full" src="${user.img}" alt="">
+      </li>
+      <li class="list-none">
+         <p class="font-semibold text-base text-black">${user.name}</p>
+      </li>
+   </a>
+   `
+})
